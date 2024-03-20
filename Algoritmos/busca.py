17ff94c52df5487bc64eabab4a18fb6bda15a536
José <jrsneto.2005@gmail.com>
2024-03-20 18:35:11 -0300

    Implementacao do metodo static busca binaria

--- Algoritmos/busca.py
+++ Algoritmos/busca.py
@@ -5,3 +5,17 @@ class Busca:
             if lista[i] == elemento:
                 return i
         return -1
+    @staticmethod
+    def busca_binaria(lista, alvo):
+        inicio = 0
+        fim = len(lista) - 1
+        
+        while inicio <= fim:
+            meio = (inicio + fim) // 2
+            if lista[meio] == alvo:
+                return meio
+            elif lista[meio] < alvo:
+                inicio = meio + 1
+            else:
+                fim = meio - 1
+        return -1

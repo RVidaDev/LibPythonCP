236506725ad7303bf1b02d8a225d9abd5e23c0c0
José <jrsneto.2005@gmail.com>
2024-03-25 17:19:45 -0300

    Implementando os testes

--- Algoritmos/ordenacao.py
+++ Algoritmos/ordenacao.py
@@ -52,6 +52,16 @@ def merge(esquerda, direita):
     resultado.extend(direita[j:])
     
     return resultado
+
+ 
+    def quicksort(self, arr):
+        if len(arr) <= 1:
+            return arr
+        else:
+            pivot = arr[0]
+            less_than_pivot = [x for x in arr[1:] if x <= pivot]
+            greater_than_pivot = [x for x in arr[1:] if x > pivot]
+            return self.quicksort(less_than_pivot) + [pivot] + self.quicksort(greater_than_pivot)
             
             
                 
---
+++ Testes/test_busca.py
@@ -0,0 +1,54 @@
+def busca_elemento(vetor, elemento):
+    for item in vetor:
+        if item == elemento:
+            return True
+        return False
+    
+    vetor = [3, 7, 33, 59, 71]
+    
+    elemento_de_busca = 33
+    
+    resultado = busca_elemento(vetor, elemento_de_busca)
+    
+#///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+def busca_elemento_nulo(vetor, elemento):
+    for item in vetor:
+        if item == elemento:
+            return True
+        return False
+    
+    vetor = [3, 7, 33, 59, 71]
+    
+    elemento_de_busca = [10]
+    
+    resultado = busca_elemento_nulo(vetor, elemento_de_busca)
+    
+#//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+def busca_elemento_vazio(vetor, elemento):
+    for item in vetor:
+        if item == elemento:
+            return True
+        return False
+    
+    vetor_vazio = []
+    
+    elemento_busca = 10
+    
+    resultado = busca_elemento_vazio(vetor_vazio, elemento_busca)
+    
+#/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+def busca_elemento_unico(vetor, elemento):
+    for item in vetor:
+        if item == elemento:
+            return True
+        return False
+    
+    vetor_unico = [3, 9, 200, 42, 29, 44 ]
+    
+    elemento_busca = [42]
+    
+    resultado = busca_elemento_unico(vetor, elemento_busca)
+    
